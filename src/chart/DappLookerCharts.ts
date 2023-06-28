9d15539da6e2054f7aebbe0e61a776ad3d76878f
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2023-06-28 08:54:04 +0530

    Reformatting code

--- src/chart/DappLookerCharts.ts
+++ src/chart/DappLookerCharts.ts
@@ -2,20 +2,13 @@ import ChartConstants from "./ChartConstants";
 
 export class DappLookerChartsAPI {
     static async getChartData(chartUUID: string, apiKey: string, format?: string) {
-        const oThis = this;
         let outputFormat = format?.toLowerCase();
-        console.log(outputFormat);
-        console.log(outputFormat !== undefined && !ChartConstants.supportedFormatType.includes(outputFormat));
-
         if ((outputFormat !== undefined && ChartConstants.supportedFormatType.includes(outputFormat)) || outputFormat === undefined) {
             let chartAPIUrl = ChartConstants.getChartDetailUrl;
             let fullAPIUrl = `${chartAPIUrl}/${chartUUID}?api_key=${apiKey}&output_format=${format}`;
-            console.log(fullAPIUrl)
+            console.log(`Calling DappLooker API: ${fullAPIUrl}`);
             let resObject = await fetch(fullAPIUrl);
-            let resJSON = await resObject.json();
-            console.log(`${JSON.stringify(resObject.status)}`);
-            console.log(`${resJSON}`);
-            return resJSON;
+            return await resObject.json();
         } else {
             return {
                 success: false,
